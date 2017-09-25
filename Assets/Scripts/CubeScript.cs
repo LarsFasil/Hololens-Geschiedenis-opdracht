3bd64fbb2364cc4c474fd141b45b00d2a62c8823
fasill001 <lars.fasil@hva.nl>
2017-09-25 12:48:28 +0200

    Gehele eerder gemaakte hololens template geimporteerd

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+	}
+
+    void Rotate()
+    {
+        this.transform.RotateAround(transform.position, Vector3.up, 20*Time.deltaTime);
+    }
+}
